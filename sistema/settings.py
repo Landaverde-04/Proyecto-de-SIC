e8db40e29a31bca64bdd90285966f869d54fe07c
Landaverde-04 <148600843+Landaverde04@users.noreply.github.com>
2024-11-07 01:02:51 -0600

    cambio 5 para render

--- sistema/settings.py
+++ sistema/settings.py
@@ -123,15 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS debe apuntar a la carpeta 'static' dentro de 'SisContable'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'SisContable', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# En producción, Render necesitará que 'STATIC_ROOT' esté configurado para recopilar archivos estáticos
+# Configuración de Whitenoise solo en producción
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
